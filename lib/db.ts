f094e1128514c8576eb88bc2b301b8d06729b933
buildbysachin <buildbysachin@gmail.com>
2026-07-22 16:13:56 +0530

    problem solved

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,29 @@
 import mongoose from "mongoose";
+import dns from "node:dns";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const connectDB = async () => {
-    await mongoose.connect(process.env.MONGODB_URI!)
-    console.log("mongoDB connected");
-}
+  try {
+    if (mongoose.connection.readyState >= 1) {
+      return;
+    }
+
+    const mongoUri = process.env.MONGODB_URI;
+
+    if (!mongoUri) {
+      throw new Error("MONGODB_URI is missing in .env.local");
+    }
+
+    await mongoose.connect(mongoUri, {
+      family: 4, // Forces Node.js to use IPv4 DNS resolution
+    });
+
+    console.log("MongoDB Connected Successfully");
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error);
+    throw error;
+  }
+};
+
 export default connectDB;
